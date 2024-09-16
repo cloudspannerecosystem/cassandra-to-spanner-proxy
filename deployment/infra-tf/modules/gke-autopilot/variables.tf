eed27c738ccc0fa623dd3f74b5c6049ba6eb842a
Mayur Kale <mayurkale@google.com>
2024-09-16 05:31:59 +0000

    fix cross warning

--- deployment/infra-tf/modules/gke-autopilot/variables.tf
+++ deployment/infra-tf/modules/gke-autopilot/variables.tf
@@ -59,7 +59,7 @@ variable "kubernetes_version" {
 
 variable "master_authorized_networks" {
   type        = list(object({ cidr_block = string, display_name = string }))
-  description = "List of master authorized networks. If none are provided, disallow external access (except the cluster node IPs, which GKE automatically whitelists)."
+  description = "List of master authorized networks. If none are provided, disallow external access (except the cluster node IPs)."
   default     = []
 }
 
